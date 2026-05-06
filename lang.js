5bbea03d0cf5384015387405e16afcb4dd048269
olehbibik-beep <oleh.bibik@gmail.com>
2026-05-06 08:25:37 +0200

    Add language dictionary for Russian and Czech

---
+++ lang.js
@@ -0,0 +1,40 @@
+export const dict = {
+    ru: {
+        // Главная страница
+        "my_report": "Мой отчет",
+        "participated": "Служил(а)",
+        "hours": "Часы",
+        "studies": "Изучения Библии",
+        "credit": "Кредит",
+        "submit_report": "Отправить отчет",
+        "this_week": "На этой неделе",
+        
+        // Меню
+        "nav_home": "Главная",
+        "nav_tasks": "Задания",
+        "nav_terr": "Участки",
+        
+        // Профиль
+        "profile_logout": "Выйти из аккаунта",
+        "language": "Язык / Jazyk"
+    },
+    cs: {
+        // Главная страница
+        "my_report": "Moje zpráva",
+        "participated": "Ve službě",
+        "hours": "Hodiny",
+        "studies": "Biblická studia",
+        "credit": "Kredit",
+        "submit_report": "Odeslat zprávu",
+        "this_week": "Tento týden",
+        
+        // Меню
+        "nav_home": "Hlavní",
+        "nav_tasks": "Úkoly",
+        "nav_terr": "Obvody",
+        
+        // Профиль
+        "profile_logout": "Odhlásit se",
+        "language": "Jazyk / Язык"
+    }
+};
