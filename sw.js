7339f4df6348cc6b27cc5870ad0a7de2a468b454
olehbibik-beep <oleh.bibik@gmail.com>
2026-05-29 09:04:54 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -41,7 +41,7 @@ self.addEventListener('notificationclick', (event) => {
 // ==========================================
 // 🔥 ГЛАВНЫЙ РУБИЛЬНИК КЭША
 // ==========================================
-const CACHE_NAME = 'gro-up-v56'; 
+const CACHE_NAME = 'gro-up-v57'; 
 
 const INITIAL_CACHED_RESOURCES = [
   './',
